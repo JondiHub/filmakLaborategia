ebcc62fba18428453518d820488401b64923be1d
jondi <jondiezb@gmail.com>
2019-05-05 11:15:24 +0000

    models + weba ikusi

---
+++ filmSite/filmak/admin.py
@@ -0,0 +1,6 @@
+from filmak.models import Filma
+from filmak.models import Bozkatzailea
+from django.contrib import admin
+
+admin.site.register(Filma)
+admin.site.register(Bozkatzailea)
--- filmSite/filmak/models.py
+++ filmSite/filmak/models.py
@@ -10,3 +10,11 @@ class Filma(models.Model):
 	generoa = models.CharField(max_length=2)
 	sinopsia = models.CharField(max_length=500)
 	bozkak = models.IntegerField()
+	def __unicode__(self):
+		return self.izenburua
+
+class Bozkatzailea(models.Model):
+	erabiltzailea = models.OneToOneField(User)
+	filmak = models.ManyToManyField(Filma)
+	def __unicode__(self):
+		return unicode(self.erabiltzailea)
